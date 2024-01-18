b1ee00fee8d8352ad3629e059304d38a2e58fdd1
pp <631843571@qq.com>
2024-01-19 01:28:34 +0800

    修改了GetService 增添了2个函数 NotFoundSession() getErrorResponse()

--- src/main/java/org/example/server/service/GetService.java
+++ src/main/java/org/example/server/service/GetService.java
@@ -1,6 +1,10 @@
 package org.example.server.service;
 
 import org.example.message.Message;
+import org.example.message.Response;
+import org.example.server.data.SessionTable;
+
+import java.util.HashMap;
 
 /**
  * 当报文是GET报文时处理报文的业务类，需要包含处理报文的方法。主要用于请求资源
@@ -15,6 +19,26 @@ public class GetService implements MethodService{
     @Override
     public Message serve(Message requestMessage) {
         // TODO
+        String SessionID;
+        try {
+            SessionID = requestMessage.getHeaders().get("SessionID");
+        } catch (NullPointerException e) {
+            return getErrorResponse();
+        }
+        if (!SessionTable.getInstance().hasSession(SessionID)) {//如果没有会话
+            return NotFoundSession();
+        }
         return null;
+    }
+    private Message getErrorResponse() {
+        return new Response(new String[]{"HTTP/1.1", "500", "Internal Server Error"}, new HashMap<>(), "Lack of header.");
+    }
+    private Message NotFoundSession() {
+        System.out.println("Can't find session in session table.");
+        String[] line;String entityBody;
+        HashMap<String, String> headers = new HashMap<>();
+        line = new String[]{"HTTP/1.1", "200", "OK"};
+        entityBody = "Can't find session in session table.";
+        return new Response(line, headers, entityBody);
     }
 }
