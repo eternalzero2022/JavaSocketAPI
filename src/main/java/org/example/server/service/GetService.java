e174f13058dc3f880e0dbe7bc978134a4d391bbe
pp <631843571@qq.com>
2024-01-19 14:39:41 +0800

    修改了GetService 完善headers.txt

--- src/main/java/org/example/server/service/GetService.java
+++ src/main/java/org/example/server/service/GetService.java
@@ -4,6 +4,9 @@ import org.example.message.Message;
 import org.example.message.Response;
 import org.example.server.data.SessionTable;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.util.Base64;
 import java.util.HashMap;
 
 /**
@@ -28,6 +31,14 @@ public class GetService implements MethodService{
         if (!SessionTable.getInstance().hasSession(SessionID)) {//如果没有会话
             return NotFoundSession();
         }
+        String URL = requestMessage.getLine()[1];
+        if(isResourceFound(URL)){
+            String entityBody = findResource(URL);
+            Message message = new Response(new String[]{"HTTP/1.1", "200", "OK"}, new HashMap<>(), entityBody);
+            return message;
+        }
+        else{  }//如果没有找到资源
+
         return null;
     }
     private Message getErrorResponse() {
@@ -38,7 +49,31 @@ public class GetService implements MethodService{
         String[] line;String entityBody;
         HashMap<String, String> headers = new HashMap<>();
         line = new String[]{"HTTP/1.1", "200", "OK"};
-        entityBody = "Can't find session in session table.";
+        entityBody = "Please log in.";
         return new Response(line, headers, entityBody);
+    }
+    private boolean isResourceFound(String URL){
+        File file = new File(URL);
+        return file.exists();
+    }
+    private String findResource(String URL){
+        try {
+            File file = new File(URL);
+            FileInputStream fis = new FileInputStream(file);
+            byte[] content = new byte[(int)file.length()];
+            fis.read(content);
+            fis.close();
+            if(URL.endsWith(".txt")){
+                return new String(content);
+            }
+            else{
+                Base64.Encoder encoder = Base64.getEncoder();
+                String encoded = encoder.encodeToString(content);
+                return encoded;
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return null;
     }
 }
