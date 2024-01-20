6af3b8942c997e671c0bde8c2f50d4bfb7872c52
Maxwell <1979829742@qq.com>
2024-01-20 15:37:07 +0800

    稍微修改了main

--- src/main/java/org/example/server/Main.java
+++ src/main/java/org/example/server/Main.java
@@ -14,15 +14,18 @@ public class Main {
      * 需要创建一个线程池，并不断监听端口，一一旦建立连接就将这个连接分配给线程池中的一个线程
      * 线程会自动调用ConnectionController中的run方法
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         ExecutorService executor = Executors.newCachedThreadPool();
+        ServerSocket serverSocket=null;
         while (true) {
             try {
-                ServerSocket serverSocket = new ServerSocket(6666);
+                serverSocket = new ServerSocket(6666);
                 Socket socket = serverSocket.accept();
                 executor.execute(new ConnectionController(socket));
             } catch (IOException e) {
                 e.printStackTrace();
+            }finally {
+                if(serverSocket!=null)serverSocket.close();
             }
         }
     }
